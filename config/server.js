06ca6deec4345e9314ef0bd9ab5ccc79b7a4be0a
gearprn <gearprn@gmail.com>
2020-07-29 01:03:14 +0700

    init starpi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '370ced8dc582f1b703737ba0c836823a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7c869a7c-c17b-45f0-986f-be54e4db2d32'
+};
